5e4511c4801a57f6d9f5ea614dea82fd2840f11a
Mrkevu <prawdziwekj@gmail.com>
2024-11-08 17:07:52 +0100

    Update main.py

--- Proxy/main.py
+++ Proxy/main.py
@@ -1,13 +1,10 @@
-# Wczytaj listę proxy z pliku
 with open("proxy_list.txt", "r") as file:
     lines = file.readlines()
 
-# Przygotuj listy do przechowywania podzielonych proxy
 http_proxies = []
 socks4_proxies = []
 socks5_proxies = []
 
-# Przetwórz każdą linię, usuwając prefix i sortując proxy do odpowiednich list
 for line in lines:
     line = line.strip()
     if line.startswith("http://"):
@@ -17,7 +14,6 @@ for line in lines:
     elif line.startswith("socks5://"):
         socks5_proxies.append(line.replace("socks5://", ""))
 
-# Zapisz podzielone proxy do plików
 with open("http.txt", "w") as file:
     file.write("\n".join(http_proxies))
 
@@ -27,4 +23,4 @@ with open("socks4.txt", "w") as file:
 with open("socks5.txt", "w") as file:
     file.write("\n".join(socks5_proxies))
 
-print("Proxy zostały podzielone na pliki: http.txt, socks4.txt, socks5.txt")
+print("Proxy teraz są oddzielone do http/socks4/socks5.txt")
